a62d92f0614ffd558cf0a08beb76dec441f50873
bhikadiyakenil2611 <88153765+bhikadiyakenil2611@users.noreply.github.com>
2024-06-20 12:13:43 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from .stopwords import *
+from .stopwords import af,ar,hy,eu,bn,br,bg,ca,zh,hr,cs,da,nl,en,eo,et,fi,fr,gl,de,el,gu,ha,he,hi,hu,id,ga,it,ja,ko,ku,la,lt,lv,ms,mr,no,fa,pl,pt,ro,ru,sk,sl,so,st,es,sw,sv,th,tl,tr,uk,ur,vi,yo,zu
